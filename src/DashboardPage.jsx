6d59ec8359f09e579663af2f2d658f735e8de491
jrowley330 <joey@thedigitaldispo.com>
2025-11-24 16:58:23 -0500

    Update DashboardPage.jsx

--- src/DashboardPage.jsx
+++ src/DashboardPage.jsx
@@ -4,7 +4,7 @@ import { supabase } from './supabaseClient';
 export default function DashboardPage() {
   const handleLogout = async () => {
     await supabase.auth.signOut();
-    window.location.href = '/'; // redirect back to login
+    window.location.href = '/';
   };
 
   return (
@@ -17,8 +17,12 @@ export default function DashboardPage() {
       <div style={{ width: '100%', height: '85vh' }}>
         <iframe
           title="Clipper Dashboards Demo Dev v1"
-          src="https://app.powerbi.com/view?r=eyJrIjoiYzUzYjlJ5..." 
-          style={{ border: 'none', width: '100%', height: '100%' }}
+          src="https://app.powerbi.com/view?r=eyJrIjoiYzUzYjI5YjMtYmZmYi00N2YzLThmZmYtZWU3YmY4OGViOWYyIiwidCI6ImQxYzU2YTYwLWRjZjItNGJhMC04ZDE5LWU0MTY0NmU2ZWFkOCIsImMiOjN9"
+          style={{
+            border: 'none',
+            width: '100%',
+            height: '100%'
+          }}
           allowFullScreen={true}
         />
       </div>
