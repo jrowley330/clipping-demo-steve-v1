ab42de8c510198ee1cea169edeabec8631a2f7dd
jrowley330 <joey@thedigitaldispo.com>
2025-12-04 16:51:18 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import LoginPage from './LoginPage.jsx';
 import DashboardPage from './DashboardPage.jsx';      // Dashboards V1 (Power BI)
 import PayoutsPage from './PayoutsPage.jsx';
 import DashboardsPageV2 from './DashboardPageV2.jsx'; // new BigQuery dashboards
+import ClippersPage from './ClippersPage.jsx'; // new BigQuery dashboards
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -44,6 +45,16 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           element={
             <App>
               <PayoutsPage />
+            </App>
+          }
+        />
+
+        {/* Clippers */}
+        <Route
+          path="/clippers"
+          element={
+            <App>
+              <ClippersPage />
             </App>
           }
         />
