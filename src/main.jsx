2aed55fc4a354ef10ada92ce9713aa6f3b7a6afd
jrowley330 <joey@thedigitaldispo.com>
2025-12-25 12:21:41 -0500

    main.jsx 2

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,11 @@ import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import App from './App.jsx';
 import LoginPage from './LoginPage.jsx';
-import DashboardPage from './DashboardPage.jsx';      // Dashboards V1 (Power BI)
+import DashboardPage from './DashboardPage.jsx';
 import PayoutsPage from './PayoutsPage.jsx';
-import DashboardsPageV2 from './DashboardPageV2.jsx'; // new BigQuery dashboards
-import ClippersPage from './ClippersPage.jsx'; // new Clippers/Users Config Page
+import DashboardsPageV2 from './DashboardPageV2.jsx';
+import ClippersPage from './ClippersPage.jsx';
 import Performance from "./pages/Performance.jsx";
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
