ae6c98f702b5f056e9f1bfffc1d8bafa7c2d2976
jrowley330 <joey@thedigitaldispo.com>
2026-01-09 16:44:26 -0500

    revert settings

--- src/Settings.jsx
+++ src/Settings.jsx
@@ -601,7 +601,7 @@ export default function SettingsPage() {
             }}
             title={s.headingText}
           >
-            {(s.headingText || "Loading...").toUpperCase()}
+            {(s.headingText || "Your Clipping Campaign").toUpperCase()}
           </span>
         </div>
 
